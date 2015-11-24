cb343cd4651ac0f7d67fe4855846eecfc56bf69f
Rakesh_Navale <rackeshnavale87@users.noreply.github.com>
2015-11-24 12:55:30 -0800

    Create Longest_increasing_subsequence_DP.java

---
+++ Longest_increasing_subsequence_DP.java
@@ -0,0 +1,38 @@
+package pkg;
+
+public class LIS_DP 
+{
+
+	public static void main(String[] args) 
+	{
+        int A[] = { 2, 5, 3, 7, 11, 8, 10, 13, 6 };
+        int n = A.length;
+        System.out.println("Length of Longest Increasing Subsequence is "+ LIS(A, n));
+	}
+
+	private static int LIS(int[] array, int n) 
+	{
+		int maxLength = 1;
+		int[] DP = new int[n];
+		DP[0] = 1;
+		int[] sol = new int[n];
+
+		for (int i = 1; i < n; i++)
+		{
+		   DP[i] = 1;
+
+		   for (int j = i - 1; j >= 0; j--)
+		   {
+			  if (DP[j] + 1 > DP[i] && array[j] < array[i])
+		      {
+		         DP[i] = DP[j] + 1;
+		      }
+		   } 
+		   if (DP[i] > maxLength)
+		   {
+		      maxLength = DP[i];
+		   }
+		}
+		return maxLength;
+	}
+}
